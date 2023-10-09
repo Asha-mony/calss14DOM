ae42ff0a6a64d63ae30839fd92ebf9d66a922898
Asha-mony <ynomasha5@gmail.com>
2023-10-10 01:14:10 +0600

    first

---
+++ dom.js
@@ -0,0 +1,38 @@
+// const student = {name: "Asha", age: 21, ID:10, Address: {presentAddress: 'Dhaka', permanentAddress: 'Rajshahi'} }
+
+// student.name
+
+
+//  // console.log(document.getElementsByTagName('h1'));
+//  const h1Cointainer = document.getElementsByTagName('h1')
+//  const byID = document.getElementById('list-container')
+//  const byClassname = document.getElementsByClassName('batch')
+//  const singleQuery = document.querySelector('h1')
+//  const allQuery = document.querySelectorAll('h1')
+
+// //    console.log(h1Cointainer);
+//   // console.log(byID);
+//   // console.log(byClassname);
+//   // console.log(singleQuery);
+//   console.log(allQuery);
+
+
+const byClassname = document.getElementsByClassName('batch')
+console.log(byClassname);
+
+// const names = ['Monir', 'Habiba', 'Mendi', 'Parvin', 'Asha']
+
+// // for(let i = 0; i < name.length; i++){
+// //     console.log(names [i]);
+// // }
+
+// for(const name of names){
+//     // console.log(name + ' ' + 'Mondol');
+// }
+
+// for (const tag of byClassname){
+//     const useStyle = tag.style.color = 'red'
+//     console.log(tag);
+// }
+
+document.getElementById('list').style.color = 'red';
